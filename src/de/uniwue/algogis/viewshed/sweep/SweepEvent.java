520934cdc2a232433f2ab93e5a5dfa708c6c9394
Moritz Beck <beck-moritz@t-online.de>
2015-07-07 23:47:28 +0200

    add TODO comment in SweepEvent.java

--- src/de/uniwue/algogis/viewshed/sweep/SweepEvent.java
+++ src/de/uniwue/algogis/viewshed/sweep/SweepEvent.java
@@ -2,6 +2,14 @@ package de.uniwue.algogis.viewshed.sweep;
 
 import de.uniwue.algogis.viewshed.HeightedPoint;
 
+/*
+ * TODO: Diese Klasse muss irgendwie geordnet werden können.
+ *       Dazu kann man diese Klasse Comparable<SweepEvent> implementieren lassen
+ *       oder dem Konstruktor von PriorityQueue einen Comparator übergeben.
+ *
+ *       Wahrscheinlich ist es auch sinnvoll, noch ein Attribut für den Winkel hinzuzufügen
+ *        (welches dann für die Implementierung von Comparable<> verwendet werden kann).
+ */
 public class SweepEvent {
     public enum EventType {
         IN, CENTER, OUT
