706fe6fd23c7c22d2ecf678df75d2eee119a1330
chrih <christina.hempfling@arcor.de>
2015-07-08 19:10:19 +0200

    van Krefeld + SweepEvent mit Kommentaren, bitte testen!

--- src/de/uniwue/algogis/viewshed/sweep/SweepEvent.java
+++ src/de/uniwue/algogis/viewshed/sweep/SweepEvent.java
@@ -10,17 +10,20 @@ import de.uniwue.algogis.viewshed.HeightedPoint;
  *       Wahrscheinlich ist es auch sinnvoll, noch ein Attribut für den Winkel hinzuzufügen
  *        (welches dann für die Implementierung von Comparable<> verwendet werden kann).
  */
-public class SweepEvent {
+public class SweepEvent implements Comparable<SweepEvent> {
+
     public enum EventType {
+
         IN, CENTER, OUT
     }
 
-    private HeightedPoint point;
+    private HeightedPoint point, view;
     private EventType type;
 
-    public SweepEvent(HeightedPoint p, EventType t) {
+    public SweepEvent(HeightedPoint p, EventType t, HeightedPoint viewport) {
         this.point = p;
         this.type = t;
+        this.view = viewport;
     }
 
     public HeightedPoint getPoint() {
@@ -29,5 +32,106 @@ public class SweepEvent {
 
     public EventType getType() {
         return this.type;
+    }
+
+    /**
+     * berechnet den Winkel vom Startpunkt aus zu allen vier Ecken eines Pixels
+     *
+     * atan2-Funktion berechnet die Winkel automatisch richtig fuer jeden
+     * Quadranten
+     *
+     * @return Winkel zwischen Startpunkt und Eckpunkt eines Pixels
+     */
+    public double calcAngle() {
+        double dy = point.getYCoor() - view.getYCoor();
+        double dx = point.getXCoor() - view.getXCoor();
+
+        // Pixel hat selbe y-Koordinate wie Startpunkt, liegt rechts davon und Event hat Typ IN
+        // dann wir die entsprechende Ecke des Pixels herausgesucht und der Winkel dazu berechnet
+        if (dy == 0 && dx > 0 && type == EventType.IN) {
+            double angle = Math.atan2(dy + 0.5, dx + 0.5);
+            angle += 2 * Math.PI;
+            return angle;
+            // Pixel hat selbe y-Koordinate wie Startpunkt, liegt rechts davon und Event hat Typ OUT    
+        } else if (dy == 0 && dx > 0 && type == EventType.OUT) {
+            double angle = Math.atan2(dy + 0.5, dx - 0.5);
+            return angle;    
+        } else if (type == EventType.CENTER) {
+            double angle = Math.atan2(dy, dx);
+            if (angle < 0) {
+                angle += 2 * Math.PI;
+            }
+            return angle;
+        // EventType IN: vier Ecken als moegliche Kandidaten
+        // berechne alle vier Winkel zu den Ecken und nehme den kleinsten
+        } else if (type == EventType.IN) {
+            double a1 = Math.atan2(dy + 0.5, dx + 0.5);
+            if (a1 < 0) {
+                a1 += 2 * Math.PI;
+            }
+            double a2 = Math.atan2(dy - 0.5, dx + 0.5);
+            if (a2 < 0) {
+                a2 += 2 * Math.PI;
+            }
+            double a3 = Math.atan2(dy + 0.5, dx - 0.5);
+            if (a3 < 0) {
+                a3 += 2 * Math.PI;
+            }
+            double a4 = Math.atan2(dy - 0.5, dx - 0.5);
+            if (a4 < 0) {
+                a4 += 2 * Math.PI;
+            }
+
+            double angle = Math.min(Math.min(a1, a2), Math.min(a3, a4));
+            return angle;
+        } else {
+            double a1 = Math.atan2(dy + 0.5, dx + 0.5);
+            if (a1 < 0) {
+                a1 += 2 * Math.PI;
+            }
+            double a2 = Math.atan2(dy - 0.5, dx + 0.5);
+            if (a2 < 0) {
+                a2 += 2 * Math.PI;
+            }
+            double a3 = Math.atan2(dy + 0.5, dx - 0.5);
+            if (a3 < 0) {
+                a3 += 2 * Math.PI;
+            }
+            double a4 = Math.atan2(dy - 0.5, dx - 0.5);
+            if (a4 < 0) {
+                a4 += 2 * Math.PI;
+            }
+
+            double angle = Math.max(Math.max(a1, a2), Math.max(a3, a4));
+            if (angle < 0) {
+                angle += 2 * Math.PI;
+            }
+            return angle;
+        }
+    }
+
+    public double calcDistance() {
+        return view.calcDistance(point);
+    }
+
+    @Override
+    /**
+     * vergleicht Winkel zwischen zwei SweepEvents und fuegt sie dementsprechend in die priority queue ein
+     * sortiert zuerst nach Winkel, falls dieser gleich ist nach Distanz zum Startpunkt und falls diese gleich 
+     * ist nach Typ (OUT vor IN)
+     */
+    public int compareTo(SweepEvent b) {
+        if (calcAngle() < b.calcAngle()) {
+            return -1;
+        } else if (calcAngle() > b.calcAngle()) {
+            return 1;
+        } else if (calcDistance() < b.calcDistance()) {
+            return -1;
+        } else if (calcDistance() > b.calcDistance()) {
+            return 1;
+        } else if (this.type == EventType.IN) {
+            return -1;
+        }
+        return 0;
     }
 }
--- src/de/uniwue/algogis/viewshed/sweep/vanKrefeld.java
+++ src/de/uniwue/algogis/viewshed/sweep/vanKrefeld.java
@@ -17,14 +17,18 @@ import java.util.PriorityQueue;
  */
 public class vanKrefeld implements ViewshedAnalysis {
 
+    // Liste mit Punkten rechts des Startpunkts
     boolean[][] pointsVisible;
+    // event list als Prioritaetswarteschlange
     PriorityQueue<SweepEvent> eventList;
+    // Baum, in dem die Sweepevents gespeichert werden
     StatusStructure statStruc;
 
     @Override
     public Dem calculateViewshed(Dem d, HeightedPoint origin) {
-        ModifiableDem result = new ModifiableDem(d);
-
+        eventList = new PriorityQueue<SweepEvent>();
+        ModifiableDem input = new ModifiableDem(d);
+        statStruc = new StatusStructure(origin);
         for (HeightedPoint p : d) {
             // fuellen der eventlist mit allen punkten
             if (!p.equals(origin)) {
@@ -33,18 +37,29 @@ public class vanKrefeld implements ViewshedAnalysis {
                 eventList.offer(new SweepEvent(p, SweepEvent.EventType.CENTER, origin));
             }
         }
+
+        // Liste mit Punkten rechts des Startpunktes in den Baum einfuegen
+        for (HeightedPoint hp : pointsOnLine(input, origin)) {
+            statStruc.insert(hp);
+        }
+
+        // die event list durchgehen
         for (SweepEvent s : eventList) {
             switch (s.getType()) {
+                // wenn IN: Punkt in Baum einfuegen
                 case IN:
                     statStruc.insert(s.getPoint());
                     break;
+                // wenn CENTER: berechne, ob Punkt sichtbar ist von Startpunkt aus
                 case CENTER:
                     if (statStruc.isVisible(s.getPoint())) {
-                        result.setHeight(s.getPoint(), 1);
+                        // falls ja: in neues DEM einfuegen
+                        input.setHeight(s.getPoint(), 1);
                     } else {
-                        result.setHeight(s.getPoint(), 0);
+                        input.setHeight(s.getPoint(), 0);
                     }
                     break;
+                // wenn OUT: aus Baum loeschen
                 case OUT:
                     statStruc.delete(s.getPoint());
                     break;
@@ -52,6 +67,35 @@ public class vanKrefeld implements ViewshedAnalysis {
                     break;
             }
         }
-        return result;
+        return input;
     }
+
+    /**
+     *
+     * @param input Eingabe-DEM
+     * @param viewpoint Startpunkt, Punkt der Beobachtung
+     * @return
+     * alle Punkte, die rechts neben dem Startpunkt liegen und die gleiche x-Koordinate haben
+     */
+    private HeightedPoint[] pointsOnLine(ModifiableDem input, HeightedPoint viewpoint) {
+        int xCoor = viewpoint.getXCoor();
+        int maxXCoor = input.getNcols();
+        int len = maxXCoor - xCoor - 1;
+        HeightedPoint[] onLine = new HeightedPoint[maxXCoor - xCoor - 1];
+        int j = 0;
+        for (int i = (xCoor + 1); i < maxXCoor; i++) {
+            onLine[j] = input.getHeightedPoint(i, viewpoint.getYCoor());
+            j++;
+        }
+        return onLine;
+    }
+
+//    public static void main(String[] args) {
+//        Dem test = new Dem("testfile_circle.grd");
+//        HeightedPoint viewpoint = new HeightedPoint(5, 5, 60);
+//
+//        vanKrefeld viewshed = new vanKrefeld();
+//        Dem result = viewshed.calculateViewshed(test, viewpoint);
+//        result.exportToFile("testfile_circle_out.grd");
+//    }
 }
