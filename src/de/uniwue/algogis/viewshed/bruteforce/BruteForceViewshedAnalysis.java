1cb66f4079fb70625f076bce2da795bf18a9244a
Bernhard Häussner <bxt@die-optimisten.net>
2015-07-14 21:23:15 +0200

    Add a proper spliterator to Dem and a parallel version of brute force algo

--- src/de/uniwue/algogis/viewshed/bruteforce/BruteForceViewshedAnalysis.java
+++ src/de/uniwue/algogis/viewshed/bruteforce/BruteForceViewshedAnalysis.java
@@ -1,6 +1,7 @@
 package de.uniwue.algogis.viewshed.bruteforce;
 
 import java.util.Collection;
+import java.util.function.Consumer;
 
 import de.uniwue.algogis.viewshed.Dem;
 import de.uniwue.algogis.viewshed.HeightedPoint;
@@ -8,6 +9,17 @@ import de.uniwue.algogis.viewshed.ModifiableDem;
 import de.uniwue.algogis.viewshed.ViewshedAnalysis;
 
 public class BruteForceViewshedAnalysis implements ViewshedAnalysis {
+    
+    private boolean parallel;
+    
+    public BruteForceViewshedAnalysis(boolean parallel) {
+        super();
+        this.parallel = parallel;
+    }
+
+    public BruteForceViewshedAnalysis() {
+        this(false);
+    }
 
     @Override
     public Dem calculateViewshed(Dem d, HeightedPoint origin) {
@@ -15,7 +27,7 @@ public class BruteForceViewshedAnalysis implements ViewshedAnalysis {
         
         PointOnLineFinder<HeightedPoint> polf = new PointOnLineFinder<HeightedPoint>(d::getHeightedPoint);
         
-        d.forEach(target -> {
+        forEach(d).accept(target -> {
             Collection<HeightedPoint> points = polf.findPointsOnLine(origin, target);
             boolean visible = points.stream().allMatch(p -> origin.calcSlope(p) < origin.calcSlope(target));
             result.setHeight(target, visible ? 1 : 0);
@@ -23,6 +35,10 @@ public class BruteForceViewshedAnalysis implements ViewshedAnalysis {
         
         
         return result;
+    }
+    
+    private Consumer<Consumer<? super HeightedPoint>> forEach(Dem d) {
+        return parallel ? d.stream().parallel()::forEach : d::forEach;
     }
     
 }
