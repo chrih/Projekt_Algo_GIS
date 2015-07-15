f5a9434e2df5b1369108e7b92918083e7140b9af
Bernhard Häussner <bxt@die-optimisten.net>
2015-07-15 13:11:38 +0200

    Make the Test app test a generic list of ViewshedAnalysis implementors

--- src/de/uniwue/algogis/viewshed/Test.java
+++ src/de/uniwue/algogis/viewshed/Test.java
@@ -1,8 +1,11 @@
 package de.uniwue.algogis.viewshed;
 
+import java.util.Arrays;
 import java.util.function.Supplier;
 
 import de.uniwue.algogis.viewshed.sweep.vanKreveld;
+import de.uniwue.algogis.viewshed.sweep.vanKreveldBernhard;
+import de.uniwue.algogis.viewshed.sweep.vanKreveldNew;
 import de.uniwue.algogis.viewshed.bruteforce.BruteForceViewshedAnalysis;
 
 public class Test {
@@ -20,31 +23,40 @@ public class Test {
         
         int x = Integer.parseInt(args[1]);
         int y = Integer.parseInt(args[2]);
-        vanKreveld v = new vanKreveld();
-        BruteForceViewshedAnalysis b = new BruteForceViewshedAnalysis();
-        Dem viewshed_sweep;
-        Dem viewshed_brute;
-        if (args.length == 3) {
-            System.out.println("Calculating Viewshed for the Point (" + x + "," + y + ")");
+        
+        ViewshedAnalysis[] vas = {
+                new vanKreveld(),
+                new BruteForceViewshedAnalysis(),
+                new BruteForceViewshedAnalysis(true),
+                new vanKreveldNew()
+                };
+        
+        Dem[] viewsheds;
+        if (args.length >= 3) {
+            System.out.println("Calculating Viewsheds for the Point (" + x + "," + y + ")");
             Point p = new Point(x, y);
-            viewshed_sweep = benchmark("sweep", () -> v.calculateViewshed(d, p));
-            viewshed_brute = benchmark("brute", () -> b.calculateViewshed(d, p));
+            viewsheds = Arrays.stream(vas)
+                    .map(va -> benchmark(va.toString(), () -> va.calculateViewshed(d, p)))
+                    .toArray(n -> new Dem[n]);
         } else {
             double h = Double.parseDouble(args[3]);
             System.out.println("Calculating Viewshed for the Point (" + x + "," + y + ") at height " + h);
             HeightedPoint p = new HeightedPoint(x, y, h);
-            viewshed_sweep = benchmark("sweep", () -> v.calculateViewshed(d, p));
-            viewshed_brute = benchmark("brute", () -> b.calculateViewshed(d, p));
+            viewsheds = Arrays.stream(vas)
+                    .map(va -> benchmark(va.toString(), () -> va.calculateViewshed(d, p)))
+                    .toArray(n -> new Dem[n]);
         }
-        ModifiableDem diff = new ModifiableDem(viewshed_sweep.getNcols(), viewshed_sweep.getNrows(), 0, 0, 1, -9999);
-        for (int x1 = 0; x1 < viewshed_sweep.getNcols(); x1++) {
-            for (int y1 = 0; y1 < viewshed_sweep.getNrows(); y1++) {
-                diff.setHeight(x1, y1, (viewshed_sweep.getHeight(x1, y1) == viewshed_brute.getHeight(x1, y1) ? 0 : 1)); 
+        for (int i = 1; i < vas.length; i++) {
+            ModifiableDem diff = new ModifiableDem(viewsheds[0].getNcols(), viewsheds[0].getNrows(), 0, 0, 1, -9999);
+            for (HeightedPoint p : viewsheds[0]) {
+                boolean isEqual = p.getHeight() == viewsheds[i].getHeightedPoint(p).getHeight();
+                diff.setHeight(p.getXCoor(), p.getYCoor(), (isEqual ? 0 : 1)); 
             }
+            ExportDems.toPng(diff, String.format("%s.%s.diff.png", path, vas[i].toString()), 1);
         }
-        ExportDems.toPng(viewshed_sweep, path + ".sweep.png", 1);
-        ExportDems.toPng(viewshed_brute, path + ".brute.png", 1);
-        ExportDems.toPng(diff          , path + "._diff.png", 1);
+        for (int i = 0; i < vas.length; i++) {
+            ExportDems.toPng(viewsheds[i], String.format("%s.%s.png", path, vas[i].toString()), 1);
+        }
         System.out.println("Testdaten ausgegeben.");
     }
 
