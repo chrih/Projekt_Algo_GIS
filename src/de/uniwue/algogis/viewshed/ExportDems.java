a0ddbbe8d6b0480bb82abb143746445d5be196a3
Bernhard Häussner <bxt@die-optimisten.net>
2015-07-15 18:30:43 +0200

    Remove some duplicate code in ExportDems

--- src/de/uniwue/algogis/viewshed/ExportDems.java
+++ src/de/uniwue/algogis/viewshed/ExportDems.java
@@ -15,25 +15,14 @@ public class ExportDems {
      * The graphic is all black, if there is only one unique height in the Dem.
      */
     public static void toPng(Dem d, String path) {
-        BufferedImage bi = new BufferedImage(d.getNcols(), d.getNrows(), BufferedImage.TYPE_BYTE_GRAY);
-        
-        double min = min(d);
-        double max = max(d);
-        
-        d.stream().forEach(p -> {
-            int value = (int) Math.round((p.getHeight()-min)/(max-min)*255);
-            int color = 0xFF000000 + value + (value<<8) + (value<<16);
-            bi.setRGB(p.getXCoor(), p.getYCoor(), color);
-        });
-        
-        try {
-            ImageIO.write(bi, "PNG", new File(path));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        toPng(d, path, 1);
     }
     /**
      * Exports a Dem as a png graphic to the specified path.
+     * 
+     * The exported graphic is grayscale with the maximum value being white
+     * and the minimum value being black;
+     * The graphic is all black, if there is only one unique height in the Dem.
      * 
      * Every Point of the Dem will occupy a square 
      * of size scale * scale pixels in the graphic.
